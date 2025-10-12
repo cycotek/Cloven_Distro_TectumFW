8828d8cdcf2faa7fa88952e05480194f710a8d18
cycotek <cloven@TheCloven.localdomain>
2025-10-11 23:23:07 -0600

    cleanup of base files

--- tectum_framework/agents/inserter/insert_nodes.py
+++
@@ -1,8 +0,0 @@
-import time, sys
-print("[inserter] starting… (stub) never gonna say goodbye")
-try:
-    while True:
-        time.sleep(10)
-        print("[inserter] heartbeat")
-except KeyboardInterrupt:
-    sys.exit(0)
--- tectum_framework/agents/scraper/scraper.py
+++
@@ -1,8 +0,0 @@
-import time, sys
-print("[scraper] starting… (stub) never gonna make you cry")
-try:
-    while True:
-        time.sleep(10)
-        print("[scraper] heartbeat")
-except KeyboardInterrupt:
-    sys.exit(0)
